99a9778e5b97d0a6c6fac27dd23139f80b86f1cf
Cristian Monti <cristian.monti@sourcesense.com>
2018-08-23 16:31:56 +0200

    aggiunstato file config

--- kubernetes/config-map-test.sh
+++ kubernetes/config-map-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/***REMOVED*** bash
-kubectl --kubeconfig=../../.kube/config.***REMOVED***-***REMOVED*** delete configmap storage-manager-conf
-kubectl --kubeconfig=../../.kube/config.***REMOVED***-***REMOVED*** create configmap storage-manager-conf --from-file=../conf/test/***REMOVED***.conf
+#kubectl --kubeconfig=../../../.kube/config.***REMOVED***-***REMOVED*** delete configmap storage-manager-conf
+kubectl --kubeconfig=../../../.kube/config.***REMOVED***-***REMOVED*** create configmap storage-manager-conf --from-file=../conf/test/***REMOVED***.conf
